d6be91a3a9c990237fb4db8b9d1d3b280fe61fa6
Junho Park <wnsghekd2006@naver.com>
2015-12-17 21:26:02 +0900

    add Timer Script

---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/GameTimer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameTimer : MonoBehaviour
+{
+    public float mSecPerDay = 60f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+    IEnumerator UpdateTime()
+    {
+        while(true)
+        {
+
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/PlayerStatus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStatus : MonoBehaviour
+{
+    private float mHP = 0f;
+    private float mHunger = 0f;
+    private float mThirsty = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
