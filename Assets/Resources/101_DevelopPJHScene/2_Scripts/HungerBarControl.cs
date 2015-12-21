844cd530b620b0804c141bae1bd3ae14be14c7a7
Junho Park <wnsghekd2006@naver.com>
2015-12-21 19:22:15 +0900

    Show Status as BarUI

---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/HungerBarControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HungerBarControl : BarControl
+{
+    public PlayerStatus mStatus = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mMaxValue = 100f;
+        mCurValue = mStatus.Hunger;
+
+        StartCoroutine(UpdateCurVal());
+
+        base.Start();
+	}
+
+    IEnumerator UpdateCurVal()
+    {
+        while (true)
+        {
+            mCurValue = mStatus.Hunger;
+
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/ThirstBarControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirstBarControl : BarControl
+{
+    public PlayerStatus mStatus = null;
+
+	// Use this for initialization
+	void Start ()
+    {
+        mMaxValue = 100f;
+        mCurValue = mStatus.Thirsty;
+
+        StartCoroutine(UpdateCurVal());
+
+        base.Start();
+	}
+
+    IEnumerator UpdateCurVal()
+    {
+        while (true)
+        {
+            mCurValue = mStatus.Thirsty;
+
+            yield return null;
+        }
+    }
+}
