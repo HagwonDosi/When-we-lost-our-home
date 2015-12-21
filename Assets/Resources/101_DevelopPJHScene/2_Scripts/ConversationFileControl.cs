f874c8503c37b21ceac7f954a2ca52d7df15d6af
Junho Park <wnsghekd2006@naver.com>
2015-12-21 21:32:04 +0900

    script ShowTextSlowly

---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/ConversationFileControl.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ConversationFileControl : MonoBehaviour
+{
+    public string mFileName = "";
+    public ShowTextSlowly mText = null;
+
+    private List<string> mListOfConvs = new List<string>();
+
+	// Use this for initialization
+	void Start ()
+    {
+        string dir = System.Environment.CurrentDirectory + "\\Assets\\Resources\\DataFiles\\" + mFileName;
+        StreamReader reader = new StreamReader(dir);
+
+        while(reader.Peek() != -1)
+        {
+            mListOfConvs.Add(reader.ReadLine());
+        }
+	}
+	
+}
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/ShowTextSlowly.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * 이 스크립트는 주어진 문자열을 mLabel에 정해진 간격으로 천천히 띄워주는 역할을 한다.
+*/
+public class ShowTextSlowly : MonoBehaviour
+{
+    public UILabel mLabel = null;
+    public bool mCanBeUsed = true;      // 현재 사용가능한가?
+    public float mSecPerLetter = 0.05f; // 글자당 초 수
+
+    private string mStrToShow = "NewLabel"; // 보여줄 전체 문자열
+	
+    public void setNewString(string fStr)
+    {
+        if(mCanBeUsed)
+        {
+            mCanBeUsed = false;
+            mStrToShow = fStr;
+            StartCoroutine(UpdateShowingString());
+        }
+        else
+        {
+            Debug.LogWarning("ShowTextSlowly at " + name + " Can't be Used.");
+        }
+    }
+
+    IEnumerator UpdateShowingString()
+    {
+        int curIdx = 0;
+        float curTime = 0f;
+
+        while(true)
+        {
+            //만약 모든 문자를 표시했다면
+            if(curIdx >= mStrToShow.Length)
+            {
+                mCanBeUsed = true;
+                StopCoroutine(UpdateShowingString());
+                break;
+            }
+            else
+            {
+                if(curTime >= mSecPerLetter)
+                {
+                    curTime -= mSecPerLetter;
+                    curIdx += 1;
+
+                    char[] str = new char[mStrToShow.Length];
+
+                    for(int i = 0; i < curIdx; i++)
+                    {
+                        str[i] = mStrToShow.ToCharArray()[i];
+                    }
+
+                    string outputStr = new string(str);
+
+                    mLabel.text = outputStr;
+                    mLabel.GetComponent<UIWidget>().MakePixelPerfect();
+                }
+                else
+                {
+                    curTime += Time.deltaTime;
+                }
+            }
+
+            yield return null;
+        }
+    }
+}
