30913521265ddfa27cb30b35010d31cc8f783667
Junho Park <wnsghekd2006@naver.com>
2016-01-03 22:13:23 +0900

    Develop Complete
    Only one case exist

---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/DebugTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugTrigger : UITrigger
+{
+    public string mLogStr = "";
+
+    public override void Act()
+    {
+        Debug.Log("Debug Trigger" + mLogStr);
+    }
+}
