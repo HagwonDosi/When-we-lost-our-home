66a5f9ea9656d23b90a61a215c007edd0b2f69da
Junho Park <wnsghekd2006@naver.com>
2016-01-08 20:18:29 +0900

    Add Doored Wall

--- Assets/Resources/101_DevelopPJHScene/2_Scripts/InteractionControl.cs
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/InteractionControl.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public enum InteractionType
 {
-    Test
+    Test,
+    Door
 }
 
 /*
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/TriggerDoor.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerDoor : UITrigger
+{
+    public Animator mAnimator = null;
+
+    public override void Act()
+    {
+        mAnimator.SetBool("Opened", !mAnimator.GetBool("Opened"));
+    }
+}
