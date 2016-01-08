5f83b5eaeb7ee01a28a634007c867d51c87b4787
Junho Park <wnsghekd2006@naver.com>
2016-01-08 15:33:54 +0900

    Fix accumulated bugs and add Model

---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/MapLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 맵을 불러오기 위한 스크립트
+/// </summary>
+public class MapLoader : MonoBehaviour
+{
+
+    #region VirtualFunctions
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+
+    #endregion
+}
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/SmooothCamera.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 카메라가 mObjec를 따라가게 하는 스크립트
+/// </summary>
+public class SmooothCamera : MonoBehaviour
+{
+    public Transform mObject = null;
+    public float mSpeed = 10f;
+    public Vector3 mOffset = Vector3.zero;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine(UpdateCo());
+	}
+	
+	IEnumerator UpdateCo()
+    {
+        while(true)
+        {
+            Vector3 pos = Vector3.Lerp(transform.position, mObject.position, Time.deltaTime * mSpeed);
+
+            pos += mOffset;
+
+            transform.position = new Vector3(pos.x, pos.y, transform.position.z);
+
+            yield return null;
+        }
+    }
+}
