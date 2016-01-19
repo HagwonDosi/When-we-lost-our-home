d13288806800692c29de31618d65e7581a8fd527
Junho Park <wnsghekd2006@naver.com>
2016-01-19 21:55:46 +0900

    add models and event for building

--- Assets/Resources/101_DevelopPJHScene/2_Scripts/EntranceTrigger.cs
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/EntranceTrigger.cs
@@ -3,9 +3,16 @@ using System.Collections;
 
 public class EntranceTrigger : UITrigger
 {
+    #region Variables
+    public Animator mAnimator = null;
+    public PlayerControl mPCon = null;
     public CollisionTrigger mCTrigger = null;
     public MapLoader mLoader = null;
     public SmooothCamera mCamera = null;
+
+    private float mSpeed = 0.01f;
+    private float mDegDif = 0f;
+    #endregion
 
     public override void Act()
     {
@@ -21,6 +28,73 @@ public class EntranceTrigger : UITrigger
         }
 
         mLoader.LoadPrefabMap(ent.mEntranceTo);
-        mCamera.GoImmediately();
+        mPCon.mCheckAni = false;
+        mAnimator.SetFloat("Speed", 1f);
+        StartCoroutine(WalkBackOutSide());
+    }
+
+    /// <summary>
+    /// 바깥에서 걸어들어간다.
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WalkBackOutSide()
+    {
+        GameObject player = GameDirector.Instance.mPlayer;
+        TweenRotation tRot = player.GetComponent<TweenRotation>();
+        tRot.enabled = true;
+        tRot.from = player.transform.localEulerAngles;
+        tRot.to = new Vector3(player.transform.localEulerAngles.x, player.transform.localEulerAngles.y - 90, player.transform.localEulerAngles.z);
+        tRot.duration = 0.3f;
+        tRot.ResetToBeginning();
+
+        mDegDif = 0 - player.transform.localEulerAngles.y;
+        
+        yield return new WaitForSeconds(0.3f);
+
+        Debug.Log(mAnimator.GetBool("Player_Run"));
+        float targetZ = player.transform.localPosition.z + 0.5f;
+        while(player.transform.localPosition.z <= targetZ)
+        {
+            Vector3 local = player.transform.localPosition;
+            local.z += Time.deltaTime * 62.5f * mSpeed;
+            player.transform.localPosition = local;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(WalkInBuilding());
+    }
+
+    /// <summary>
+    /// 건물 안으로 걸어들어간다
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator WalkInBuilding()
+    {
+        mLoader.CurBuiding.GetComponent<BuildingControl>().SetPlayer();
+        GameObject player = GameDirector.Instance.mPlayer;
+        float targetZ = player.transform.localPosition.z + 0.5f;
+        while (player.transform.localPosition.z <= targetZ)
+        {
+            Vector3 local = player.transform.localPosition;
+            local.z += Time.deltaTime * 62.5f * mSpeed;
+            player.transform.localPosition = local;
+
+            yield return null;
+        }
+
+        TweenRotation tRot = player.GetComponent<TweenRotation>();
+        tRot.enabled = true;
+        tRot.from = player.transform.localEulerAngles;
+        tRot.to = new Vector3(player.transform.localEulerAngles.x, player.transform.localEulerAngles.y + 90, player.transform.localEulerAngles.z);
+        tRot.duration = 0.3f;
+        tRot.ResetToBeginning();
+
+        yield return new WaitForSeconds(0.3f);
+
+        mAnimator.SetFloat("Speed", 0f);
+        mPCon.mCheckAni = true;
+        StopAllCoroutines();
     }
 }
---
+++ Assets/Resources/101_DevelopPJHScene/2_Scripts/QuitTrigger.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitTrigger : UITrigger
+{
+    public PlayerControl mPCon = null;
+    public Animator mAnimatr = null;
+    public SmooothCamera mCamera = null;
+
+    private float mSpeed = 0.01f;
+    private float mDegDif = 0f;
+    private BuildingControl mBuilCon = null;
+    private TweenRotation mTRot = null;
+    private GameObject mPlayer = null;
+
+    public override void Act()
+    {
+        mBuilCon = FindObjectOfType<BuildingControl>();
+        mPlayer = GameDirector.Instance.mPlayer;
+        mTRot = mPlayer.GetComponent<TweenRotation>();
+        mPCon.mCheckAni = false;
+
+        mAnimatr.SetFloat("Speed", 1f);
+        mTRot.enabled = true;
+        mTRot.from = mPlayer.transform.localEulerAngles;
+        mTRot.to = new Vector3(mPlayer.transform.localEulerAngles.x, 180, mPlayer.transform.localEulerAngles.z);
+        mTRot.duration = 0.3f;
+        mTRot.ResetToBeginning();
+
+        mDegDif = 180f - mPlayer.transform.localEulerAngles.y;
+        StartCoroutine(WalkFront1());
+    }
+
+    private IEnumerator WalkFront1()
+    {
+        yield return new WaitForSeconds(0.3f);
+
+        float targetZ = mPlayer.transform.localPosition.z - 0.5f;
+        while (mPlayer.transform.localPosition.z >= targetZ)
+        {
+            Vector3 local = mPlayer.transform.localPosition;
+            local.z -= Time.deltaTime * 62.5f * mSpeed;
+            mPlayer.transform.localPosition = local;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        StopAllCoroutines();
+        StartCoroutine(WalkFront2());
+    }
+
+    private IEnumerator WalkFront2()
+    {
+        mBuilCon.PlayerExit();
+
+        float targetZ = mPlayer.transform.localPosition.z - 0.5f;
+        while (mPlayer.transform.localPosition.z >= targetZ)
+        {
+            Vector3 local = mPlayer.transform.localPosition;
+            local.z -= Time.deltaTime * 62.5f * mSpeed;
+            mPlayer.transform.localPosition = local;
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        mTRot.enabled = true;
+        mTRot.from = mPlayer.transform.localEulerAngles;
+        mTRot.to = new Vector3(mPlayer.transform.localEulerAngles.x, 180f - mDegDif, mPlayer.transform.localEulerAngles.z);
+        mTRot.duration = 0.3f;
+        mTRot.ResetToBeginning();
+
+        yield return new WaitForSeconds(0.3f);
+        StopAllCoroutines();
+        mPCon.mCheckAni = true;
+
+        mAnimatr.SetFloat("Speed", 0f);
+    }
+}
